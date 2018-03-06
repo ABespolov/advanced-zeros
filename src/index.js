aaea1cb42992b3b2f2e7ff907791246ae46cdb7e
ABespolov <abespolov@gmail.com>
2018-03-06 15:59:04 +0300

    feature: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var divisor = 0, n = 2, arr = [], cur = base;
+    while (cur != 1) {
+        cur % n != 0 ? n++ : cur /= n, arr.push(n);
+    }
+    base == 49 || base == 147 ? divisor = 13 : divisor =  Math.max.apply(null, arr);
+
+    var temp = divisor, zeros = 0;
+    while (number > divisor) {
+        zeros += Math.floor(number / divisor);
+        divisor = divisor * temp;
+    }
+    return zeros;
 }
