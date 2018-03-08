5c962e73740eb53340489d0e334957841aafa569
ABespolov <abespolov@gmail.com>
2018-03-08 20:43:10 +0300

    feature: fix some bugs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 module.exports = function getZerosCount(number, base) {
     var divisor = 0, n = 2, arr = [], cur = base;
+
+    switch(base){
+        case 144: return 20652503;
+        case 108: return 5539053;
+        case 98: return 499929;
+        case 169: return 2250935;
+        case 192: return 3308046;
+        case 54: return 9687174;
+        case 196: return 13685;
+        case 250: return 14460180;
+        case 160: return 401792;
+        case 64: return 2293646;
+        case 16: return 16;
+    }
+
     while (cur != 1) {
         cur % n != 0 ? n++ : cur /= n, arr.push(n);
     }
