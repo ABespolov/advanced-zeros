56a9e492fd8677d638e60b4e44dfb3801d591dab
Artem Bespolov <abespolov@gmail.com>
2018-09-12 07:44:09 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 module.exports = function getZerosCount(number, base) {
-//function getZerosCount(number, base) {
 
     function findPrimeNumbers(base) {
         var dividers = [];
@@ -20,7 +19,7 @@ module.exports = function getZerosCount(number, base) {
     function findPrimeDividers(base) {
         var primeNumbers = findPrimeNumbers(base);
         var primeDividers = {};
-        
+
         for (var i = 0; i < primeNumbers.length; i++) {
             while (base % primeNumbers[i] == 0) {
                 var primeDivider = base / primeNumbers[i];
@@ -32,41 +31,31 @@ module.exports = function getZerosCount(number, base) {
                 }
             }
         }
-        if(Object.keys(primeDividers).length == 0){
+        if (Object.keys(primeDividers).length == 0) {
             primeDividers[base] = 1;
         }
         return primeDividers;
     }
 
-
-    
-
     var dividers = findPrimeDividers(base);
     var sums = JSON.parse(JSON.stringify(dividers));
     var currNumber = number;
 
-    console.log(dividers)
-
-    for(var key in dividers){
+    for (var key in dividers) {
         var d = +key;
         sums[key] = 0;
-        for(var i = 1; i <= Math.pow(key, dividers[key]); i++){
+        for (var i = 1; number / d >= 1; i++) {
             sums[key] += Math.floor(number / d);
-            d = d * +key;      
+            d = d * +key;
         }
-       // sums[key] = Math.floor(sums[key] / dividers[key]);
+        sums[key] = Math.floor(sums[key] / dividers[key]);
         number = currNumber;
-        
-    }
 
-    var zeros = 9999999999999999999999999999;
-    for(var key in sums){
-        if(sums[key] < zeros) zeros = sums[key];
+    }
+    var zeros = Number.MAX_SAFE_INTEGER;
+    for (var key in sums) {
+        if (sums[key] < zeros) zeros = sums[key];
     }
 
     return zeros;
-
-    console.log(sums)
-    console.log(zeros)
 }
-
